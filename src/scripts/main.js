4892e22ef0ffba3a4d8137f1b10a4c0f4e16dc9d
Eddie Kao <eddie@5xcampus.com>
2025-04-08 14:00:55 +0800

    feat: add user signin status

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,54 @@
 import axios from "axios"
 import Swal from "sweetalert2"
 
+const TOKEN_NAME = "user_token"
+
 const Main = () => ({
   showSection: "loginSection",
   email: "",
   nickname: "",
   password: "",
-  showLogin() {
-    this.showSection = "loginSection"
+  isLogin: false,
+  init() {
+    const token = localStorage.getItem(TOKEN_NAME)
+    if (token) {
+      this.isLogin = true
+      this.showTaskInput()
+    }
   },
   clearText() {
     this.email = ""
     this.nickname = ""
     this.password = ""
   },
+  showLogin() {
+    this.showSection = "loginSection"
+  },
   showSignUp() {
     this.showSection = "signUpSection"
+  },
+  showTaskInput() {
+    this.showSection = "taskSection"
+  },
+  async logout() {
+    // 使用 delete 登出的 API
+    const url = "https://todoo.5xcamp.us/users/sign_out"
+    // 傳 token 給 API
+    const token = localStorage.getItem(TOKEN_NAME)
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = token
+      const resp = await axios.delete(url)
+      console.log(resp)
+    }
+
+    // 成功：
+    //   isLogin = false
+    //   清 localStorage
+    //   換畫面到登入頁面
+    // 失敗：
+    //   isLogin = false
+    //   清 localStorage
+    //   換畫面到登入頁面
   },
   async login() {
     if (this.email != "" && this.password != "") {
@@ -30,9 +63,10 @@ const Main = () => ({
         const resp = await axios.post("https://todoo.5xcamp.us/users/sign_in", userData)
         const token = resp.headers.authorization
 
-        localStorage.setItem("user_token", token)
+        localStorage.setItem(TOKEN_NAME, token)
         this.clearText()
-        console.log("ok")
+        this.isLogin = true
+        this.showTaskInput()
       } catch (err) {
         console.log(err)
       }
